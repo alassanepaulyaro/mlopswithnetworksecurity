2e32a8251c8fdf017feada6ce5dc07a720b30d17
alassanepaulyaro <alassane-paul.yaro@outlook.fr>
2025-01-17 14:36:34 +0100

    Model Pusher

--- networksecurity/components/data_transformation.py
+++ networksecurity/components/data_transformation.py
@@ -37,7 +37,7 @@ class DataTransformation:
         
     def get_data_transformer_object(cls)->Pipeline:
         """
-        It initialises a KNNImputer object with the parameters specified in the training_pipeline.py file
+        It initializes a KNNImputer object with the parameters specified in the training_pipeline.py file
         and returns a Pipeline object with the KNNImputer object as the first step.
 
         Args:
@@ -87,7 +87,8 @@ class DataTransformation:
             train_arr = np.c_[transformed_input_train_feature, np.array(target_feature_train_df) ]
             test_arr = np.c_[ transformed_input_test_feature, np.array(target_feature_test_df) ]
 
-            #save numpy array data
+            #model pusher
+            #save numpy array data 
             save_numpy_array_data( self.data_transformation_config.transformed_train_file_path, array=train_arr, )
             save_numpy_array_data( self.data_transformation_config.transformed_test_file_path,array=test_arr,)
             save_object( self.data_transformation_config.transformed_object_file_path, preprocessor_object,)
